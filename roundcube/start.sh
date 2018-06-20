30b9c7c85b2ca8dafe95cf99cef621e1b442b615
6ec123321 <artur@phpchain.ru>
2018-06-20 11:01:21 +0400

    Removed memcached and php7-memcached

--- roundcube/start.sh
+++ roundcube/start.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-/usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf && /usr/bin/memcached -u memcached -d && /usr/sbin/php-fpm7
+#/usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf && /usr/bin/memcached -u memcached -d && /usr/sbin/php-fpm7
+/usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf && /usr/sbin/php-fpm7
--- run-rcmail136.sh
+++ run-rcmail136.sh
@@ -1,1 +1,1 @@
-docker run -d --name rcmail136_19062018 -p 8443:443 -v /home/roundcube/config:/home/roundcube/config --restart unless-stopped rcmail/rcmail:latest
+docker run -d --name rcmail136_20062018 -p 8443:443 -v /home/roundcube/config:/home/roundcube/config --restart unless-stopped rcmail/rcmail:latest
