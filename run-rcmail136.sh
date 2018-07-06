1e76f4fa623b0b943a1089643ad1661f845ef4c9
6ec123321 <artur@phpchain.ru>
2018-07-06 09:37:16 +0400

    Removed managesieve/config.inc.php

--- run-rcmail136.sh
+++ run-rcmail136.sh
@@ -1,1 +1,1 @@
-docker run -d --name rcmail136 -p 8443:443 -e MAX_UPLOAD=25M -v /home/roundcube/config:/home/roundcube/config --restart unless-stopped rcmail/rcmail:latest
+docker run -d --name rcmail136 -p 8443:443 -v /home/roundcube/config:/home/roundcube/config --restart unless-stopped rcmail/rcmail:latest
